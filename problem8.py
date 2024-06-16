d26de42d230fbe296b10a66d43e28d1c3d07247a
maty <matty.pzr@outlook.com>
2024-06-16 14:06:05 +0200

    testing new branch

---
+++ problem8.py
@@ -0,0 +1,39 @@
+# Problem Description
+"""
+Your task is to create a game called guess the number.
+Why is playing ? Computer vs Human. Computer has the role of
+choosing a number from range specified by user. (User at the start
+of the program chooses the range i.e. 0 - 100). After user chooses
+a range he is greeted by a message saying something like "Welcome to,
+this game blah blah...". Then the game may begin. Whole game is inside
+of while loop that first asks a user for a number. (remember to convert
+the input to int) and then computer decides whether the user guessed the
+number or if its lower or higher. If user guesses he wins and there is
+celebration message with congratulation to the winner.
+"""
+
+
+
+# Can you do better ? 1
+"""
+Create a certain amount of lives for each user lets say 10. If the user misses the guess,
+his lives will decrease by one. If there are no lives remaining he lost and a message game
+over prints.
+"""
+
+# Can you do better ? 2
+"""
+Edit the script so the computer is a cheater. But the user can never figure it out.
+- And the start of program computer may chose some number that can be later changed.
+
+Example:
+    Start:
+        - user range 0 - 100
+        - computer -> 72
+    Loop:
+        - user choice: 70
+        - computer -> lower and stick to a number 50 for example
+"""
+
+
+
